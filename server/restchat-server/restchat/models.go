c6040b21183951cc6d6ca55d29e327113c180470
Sergey <samurai-2006@yandex.ru>
2022-01-31 18:15:52 +0300

    Добавил Модели (UserModel, SessionModel,MessageModel))

---
+++ server/restchat-server/restchat/models.go
@@ -0,0 +1,22 @@
+package model
+
+import "time"
+
+type UserModel struct {
+	ID           int    `json:"id"`
+	Username     string `json:"username"`
+	PasswordHash string `json:"password"`
+}
+
+type SessionModel struct {
+	ID         int    `json:"id"`
+	Username   string `json:"name"`
+	Auth_token string `json:"auth_token"`
+}
+
+type MessageModel struct {
+	ID          int       `json:"id"`
+	UserId      int       `json:",omitempty"`
+	Text        string    `json:"text"`
+	TimeMessage time.Time `json:"time"` //utc
+}
