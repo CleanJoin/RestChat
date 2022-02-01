962ca835e1839962e7a2164a55ebefd9cb8f9f27
Sergey <samurai-2006@yandex.ru>
2022-02-01 13:51:20 +0300

    Добавил структуру, интерфейс, и метод
    Добавил:
    Структуру: PasswordHasherSha1
    Интерфейс:  IPasswordHasher c методом (CalculateHash)
    Реализовал метод CalculateHash

---
+++ server/restchat-server/restchat/password_hasher.go
@@ -0,0 +1,25 @@
+package restchat
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+)
+
+type PasswordHasherSha1 struct {
+	PasswordSha1 string
+}
+type IPasswordHasher interface {
+	CalculateHash(password string) string
+}
+
+func (*PasswordHasherSha1) CalculateHash(password string) (string, error) {
+	if password == "" {
+		return "", fmt.Errorf("%s", "Пароль пустой")
+
+	}
+	sha1 := sha1.New()
+	sha1.Write([]byte(password))
+	sha1_hash := hex.EncodeToString(sha1.Sum(nil))
+	return sha1_hash, fmt.Errorf("хэш создан: %s", sha1_hash)
+}
