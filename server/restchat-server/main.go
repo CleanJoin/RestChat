2f5f4321c9a818d0d18ea0a35c4d4630db8f211c
min0ru <2010min0ru@gmail.com>
2022-02-03 21:30:01 +0300

    Пример передачи параметра (хранилища сессий) в хэндлер веб-сервера

--- server/restchat-server/main.go
+++ server/restchat-server/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"restchat-server/restchat"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello, Golang!")
+
+	session_storage := restchat.NewSessionStorageMemory(
+		new(restchat.TokenGeneratorUUID),
+	)
+
+	session_storage.Create(1000)
+	session_storage.Create(2000)
 
 	router := gin.Default()
 	router.GET("/", func(ctx *gin.Context) {
@@ -17,6 +24,7 @@ func main() {
 			"message": "Kill",
 		})
 	})
+
 	router.GET("/user", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"error":   "Not implemented YET",
@@ -24,5 +32,17 @@ func main() {
 		})
 	})
 
+	router.GET("/login", login_handler(session_storage))
+
 	router.Run()
+}
+
+func login_handler(session_storage restchat.ISessionStorage) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		session, _ := session_storage.Create(4000)
+		fmt.Println("handler func:", session.Auth_token)
+		ctx.JSON(http.StatusOK, gin.H{
+			"api_token": session.Auth_token,
+		})
+	}
 }
