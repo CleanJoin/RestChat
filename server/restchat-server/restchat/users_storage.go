b5bf476f026953f0d81e4815f5c9c48bd6839c33
Sergey <samurai-2006@yandex.ru>
2022-02-04 16:07:23 +0300

    Написал test и реализацию методов и функций
    #9

--- server/restchat-server/restchat/users_storage.go
+++ server/restchat-server/restchat/users_storage.go
@@ -1,9 +1,13 @@
 package restchat
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type UserStorageMemory struct {
-	Users []UserModel
+	Users          []UserModel
+	passwordHasher IPasswordHasher
 }
 
 type IUserStorage interface {
@@ -13,31 +17,62 @@ type IUserStorage interface {
 	GetByIds(ids []uint) []UserModel
 }
 
-func NewUserStorageMemory() *UserStorageMemory {
-	return &UserStorageMemory{}
+func NewUserStorageMemory(passwordHasher IPasswordHasher) *UserStorageMemory {
+	ssm := new(UserStorageMemory)
+	ssm.passwordHasher = passwordHasher
+	return ssm
+}
+func getLastUserId(usm *UserStorageMemory) uint {
+	if usm == nil || len(usm.Users) == 0 {
+		return 0
+	}
+	sort.Slice(usm.Users, func(i, j int) (less bool) {
+		return usm.Users[i].ID > usm.Users[j].ID
+	})
+	return usm.Users[0].ID
 }
 
 func (usm *UserStorageMemory) Create(username string, password string) (UserModel, error) {
-	return UserModel{ID: 0, Username: "", PasswordHash: ""}, nil
+	id := getLastUserId(usm)
+	id++
+	lenlastuser := len(usm.Users)
+	passwordHash := usm.passwordHasher.CalculateHash(password)
+	usm.Users = append(usm.Users, UserModel{ID: id, Username: username, PasswordHash: passwordHash})
+	if lenlastuser >= len(usm.Users) {
+		return UserModel{ID: 0, Username: "", PasswordHash: ""}, fmt.Errorf("%s", "Не удалось добавить сообщение")
+	}
+	return usm.Users[len(usm.Users)-1], nil
 }
+
 func (usm *UserStorageMemory) GetByName(username string) (UserModel, error) {
 	for i, r := range usm.Users {
 		if r.Username == username {
-			return usm.Users[i], fmt.Errorf("нашелся пользователь: %v", usm.Users[i])
+			return usm.Users[i], nil
 		}
 	}
-	return UserModel{ID: 0, Username: "", PasswordHash: ""}, nil
+	return UserModel{ID: 0, Username: "", PasswordHash: ""}, fmt.Errorf("не нашелся пользователь по указанному Username: %v", username)
 }
 
 func (usm *UserStorageMemory) GetById(id uint) (UserModel, error) {
 	for i, r := range usm.Users {
 		if r.ID == id {
-			return usm.Users[i], fmt.Errorf("нашелся пользователь: %v", usm.Users[i])
+			return usm.Users[i], nil
 		}
 	}
-	return UserModel{ID: 0, Username: "", PasswordHash: ""}, nil
+	return UserModel{ID: 0, Username: "", PasswordHash: ""}, fmt.Errorf("пользователь с указанным id:%v не нашелся ", id)
 }
 
-// func (usm *UserStorageMemory) GetByIds(ids []uint) ([]UserModel, error) {
-// 	return  fmt.Errorf("%s", "Не нашелся пользователь:")
-// }
+func (usm *UserStorageMemory) GetByIds(ids []uint) ([]UserModel, error) {
+	idIndexer := make(map[uint]uint)
+	newUserStorage := new(UserStorageMemory)
+	for index, user := range usm.Users {
+		idIndexer[user.ID] = uint(index)
+	}
+	for _, id := range ids {
+		if val, ok := idIndexer[id]; ok {
+			newUserStorage.Users = append(newUserStorage.Users, UserModel{ID: id, Username: usm.Users[val].Username, PasswordHash: usm.Users[val].PasswordHash})
+		}
+
+	}
+	return newUserStorage.Users, nil
+}
--- server/restchat-server/restchat/users_storage_test.go
+++ server/restchat-server/restchat/users_storage_test.go
@@ -1,1 +1,85 @@
 package restchat
+
+import (
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func TestIUserStorage(t *testing.T) {
+	inter := NewUserStorageMemory(new(PasswordHasherSha1))
+
+	user, err := inter.Create("Андрей", "Андрей")
+	if err != nil {
+		t.Errorf("Не создался пользователь %v", err)
+	}
+	_, err = inter.GetById(user.ID)
+	if err != nil {
+		t.Errorf("Не получили пользователя по id %v", err)
+	}
+	_, err = inter.GetByName(user.Username)
+	if err != nil {
+		t.Errorf("Не получили пользователя по username %v", err)
+	}
+	ids := []uint{1, 2, 3, 4, 4}
+	_, err = inter.GetByIds(ids)
+	if err != nil {
+		t.Errorf("Не получили пользователя по указанным идентификаторам %v", err)
+	}
+
+}
+func TestCreateUser(t *testing.T) {
+	usm := NewUserStorageMemory(new(PasswordHasherSha1))
+	_, err := usm.Create("Андрей", "Андрей")
+	if err != nil {
+		t.Errorf("Не создался пользователь %v", err)
+	}
+}
+func TestGetLastUserId(t *testing.T) {
+	usm := NewUserStorageMemory(new(PasswordHasherSha1))
+	_, err := usm.Create("Андрей", "Андрей")
+	if err != nil {
+		t.Errorf("Не создался пользователь %v", err)
+	}
+	if getLastUserId(usm) != usm.Users[0].ID {
+		t.Errorf("Не получили идентификатор последнего пользователя")
+	}
+}
+
+func TestGetByName(t *testing.T) {
+	usm := NewUserStorageMemory(new(PasswordHasherSha1))
+	user, err := usm.Create("Андрей", "Андрей")
+	if err != nil {
+		t.Errorf("Не создался пользователь %v", err)
+	}
+	_, err = usm.GetByName(user.Username)
+	if err != nil {
+		t.Errorf("не нашелся пользователь по указанному Username %v", err)
+	}
+}
+func TestGetById(t *testing.T) {
+	usm := NewUserStorageMemory(new(PasswordHasherSha1))
+	user, err := usm.Create("Андрей", "Андрей")
+	if err != nil {
+		t.Errorf("Не создался пользователь %v", err)
+	}
+	_, err = usm.GetById(user.ID)
+	if err != nil {
+		t.Errorf("пользователь с указанным id не нашелся %v", err)
+	}
+}
+func TestGetByIds(t *testing.T) {
+	usm := NewUserStorageMemory(new(PasswordHasherSha1))
+	for i := 1; i < 10; i++ {
+		usm.Create("Андрей"+strconv.Itoa(i), "Андрей"+strconv.Itoa(i))
+	}
+	ids := []uint{2, 3, 6, 7}
+	request, err := usm.GetByIds(ids)
+	if err != nil {
+		t.Errorf("не нашелся пользователь по указанному Username %v", err)
+	}
+	outUserModel := []UserModel{{ID: 2, Username: "Андрей2", PasswordHash: request[0].PasswordHash}, {ID: 3, Username: "Андрей3", PasswordHash: request[1].PasswordHash}, {ID: 6, Username: "Андрей6", PasswordHash: request[2].PasswordHash}, {ID: 7, Username: "Андрей7", PasswordHash: request[3].PasswordHash}}
+	if reflect.DeepEqual(request, outUserModel) != true {
+		t.Errorf("Не верный список пользователей %v %v", request, outUserModel)
+	}
+}
