5ee32928afb66c86201b7b14e82a680fddc0b912
min0ru <2010min0ru@gmail.com>
2022-02-06 23:23:11 +0300

    Функциональный тест доступности тестовой инфраструктуры.

---
+++ functional_tests/cypress/integration/0-integration-smoke/smoke.spec.js
@@ -0,0 +1,8 @@
+describe('integration smoke tests', () => {
+
+    it('application proxy is accessible', () => {
+        cy.visit('http://localhost:10000');
+        cy.request('http://localhost:10000/api/ping');
+    });
+
+});
