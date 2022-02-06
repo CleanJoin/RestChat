e25ffc8e7c9d4de1784ddb7bef0f69f07af4413d
min0ru <2010min0ru@gmail.com>
2022-02-06 23:30:11 +0300

    Изменен эндпоинт для проверки состояния бэкенда.

--- functional_tests/cypress/integration/0-integration-smoke/smoke.spec.js
+++ functional_tests/cypress/integration/0-integration-smoke/smoke.spec.js
@@ -2,7 +2,7 @@ describe('integration smoke tests', () => {
 
     it('application proxy is accessible', () => {
         cy.visit('http://localhost:10000');
-        cy.request('http://localhost:10000/api/ping');
+        cy.request('http://localhost:10000/api/healthcheck');
     });
 
 });
