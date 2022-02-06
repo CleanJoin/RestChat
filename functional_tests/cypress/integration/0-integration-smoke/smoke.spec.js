019fc3bf52e981d2b0f8e61dd6298643a214d7a4
min0ru <2010min0ru@gmail.com>
2022-02-06 23:57:20 +0300

    Расширен интеграционный тест.

--- functional_tests/cypress/integration/0-integration-smoke/smoke.spec.js
+++ functional_tests/cypress/integration/0-integration-smoke/smoke.spec.js
@@ -1,8 +1,20 @@
+import { StatusCodes } from 'http-status-codes';
+
 describe('integration smoke tests', () => {
 
-    it('application proxy is accessible', () => {
+    it('application frontend is accessible through proxy', () => {
         cy.visit('http://localhost:10000');
-        cy.request('http://localhost:10000/api/health');
     });
 
+    it('application backend is accessible through proxy', () => {
+        cy.request('http://localhost:10000/api/');
+    });
+
+    it('backend successfully returns health status', () => {
+        cy.request('http://localhost:10000/api/health').should((response) => {
+            expect(response.status).to.be.equal(StatusCodes.OK);
+            expect(response.body).to.have.property("success").equal(true);
+            expect(response.body).to.have.property("time");
+        });
+    });
 });
