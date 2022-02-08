5b0aa9f247c339bd5da0b8f62394ae2e3f6df587
Sergey <samurai-2006@yandex.ru>
2022-02-08 17:04:15 +0300

    Переделал main.go
    Для теста  и запуска сервиса

--- server/restchat-server/main.go
+++ server/restchat-server/main.go
@@ -4,45 +4,47 @@ import (
 	"fmt"
 	"net/http"
 	"restchat-server/restchat"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	session_storage := restchat.NewSessionStorageMemory(
-		new(restchat.TokenGeneratorUUID),
-	)
+	// session_storage := restchat.NewSessionStorageMemory(
+	// 	new(restchat.TokenGeneratorUUID),
+	// )
 
-	session_storage.Create(1000)
-	session_storage.Create(2000)
+	chatServerGin := restchat.NewChatServerGin("localhost", 8080, 300)
+	sessionStorage1 := restchat.NewSessionStorageMemory(new(restchat.TokenGeneratorUUID))
+	usersstorage := restchat.NewUserStorageMemory(new(restchat.PasswordHasherSha1))
+	messageStorage := restchat.NewMessageStorageMemory()
+	chatServerGin.Use(sessionStorage1, usersstorage, messageStorage)
+	chatServerGin.Run()
+	// router := gin.Default()
+	// router.GET("/api/", func(ctx *gin.Context) {
+	// 	ctx.JSON(http.StatusOK, gin.H{
+	// 		"error":   "Not implemented YET",
+	// 		"message": "Kill",
+	// 	})
+	// })
 
-	router := gin.Default()
-	router.GET("/api/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"error":   "Not implemented YET",
-			"message": "Kill",
-		})
-	})
+	// router.GET("/api/user", func(ctx *gin.Context) {
+	// 	ctx.JSON(http.StatusOK, gin.H{
+	// 		"error":   "Not implemented YET",
+	// 		"message": "What your name?",
+	// 	})
+	// })
 
-	router.GET("/api/user", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"error":   "Not implemented YET",
-			"message": "What your name?",
-		})
-	})
+	// router.GET("/api/login", login_handler(session_storage))
 
-	router.GET("/api/login", login_handler(session_storage))
+	// router.GET("/api/health", func(ctx *gin.Context) {
+	// 	ctx.JSON(http.StatusOK, gin.H{
+	// 		"success": true,
+	// 		"time":    time.Now().Format(time.RFC3339),
+	// 	})
+	// })
 
-	router.GET("/api/health", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"success": true,
-			"time":    time.Now().Format(time.RFC3339),
-		})
-	})
-
-	router.Run()
+	// router.Run()
 }
 
 func login_handler(session_storage restchat.ISessionStorage) gin.HandlerFunc {
