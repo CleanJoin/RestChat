871a3f2f06ed870daadffc29ac1d939e73c8f9e0
min0ru <2010min0ru@gmail.com>
2022-02-10 23:34:14 +0300

    Тесты методов, требующих авторизации.

--- client/src/ApiClient/ApiClientRest.test.js
+++ client/src/ApiClient/ApiClientRest.test.js
@@ -3,8 +3,41 @@ import ApiClientRest from './ApiClientRest';
 describe('ApiClientRest authorization requirements', () => {
 
     test('client instance created unauthorized', () => {
-        const apiClient = new ApiClientRest()
-        expect(apiClient.isAuthorized()).toEqual(false);
+        const client = new ApiClientRest();
+        expect(client.isAuthorized()).toEqual(false);
     });
 
-})
+    test('requireAuthorization() throws error', () => {
+        const client = new ApiClientRest();
+        expect(() => client.requireAuthorization()).toThrow();
+    });
+
+    test('logout() requires authorization', async () => {
+        const client = new ApiClientRest();
+        await expect(async () => {
+            await client.logout();
+        }).rejects.toThrowError(/not authorized/i);
+    });
+
+    test('getMembers() requires authorization', async () => {
+        const client = new ApiClientRest();
+        await expect(async () => {
+            await client.getMembers();
+        }).rejects.toThrowError(/not authorized/i);
+    });
+
+    test('getMessages() requires authorization', async () => {
+        const client = new ApiClientRest();
+        await expect(async () => {
+            await client.getMessages();
+        }).rejects.toThrowError(/not authorized/i);
+    });
+
+    test('sendMessage() requires authorization', async () => {
+        const client = new ApiClientRest();
+        await expect(async () => {
+            await client.getMessages();
+        }).rejects.toThrowError(/not authorized/i);
+    });
+
+});
