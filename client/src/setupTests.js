ce7abf8f9490cb10d7bd9cdb9569d7ebd702cb80
min0ru <2010min0ru@gmail.com>
2022-02-11 12:10:42 +0300

    Инициализация mockApi перенесена в глобальную область видимости тестов.

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import mockApiServer from './MockApi/server'
+
+const mockServer = mockApiServer;
+
+beforeAll(() => {
+    mockServer.listen();
+});
+
+afterEach(() => {
+    mockServer.resetHandlers();
+});
+
+afterAll(() => {
+    mockServer.close();
+});
