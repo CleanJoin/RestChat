c6789eada644805642ee0809b92864b587136057
min0ru <2010min0ru@gmail.com>
2022-02-15 12:24:08 +0300

    Исправления в sql скрипте.

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -1,29 +1,34 @@
 CREATE SCHEMA "UserModel" AUTHORIZATION "restchat";
 
 CREATE TABLE "UserModel".messages (
-	id int4 NOT NULL,
+	id integer NOT NULL,
 	"text" varchar NOT NULL,
-	user_id int4 NOT NULL,
+	user_id integer NOT NULL,
 	"time" timestamptz NOT NULL
 );
+
 CREATE UNIQUE INDEX messages_user_id_idx ON "UserModel".messages USING btree (user_id);
 
 ALTER TABLE "UserModel".messages OWNER TO "restchat";
+
 GRANT ALL ON TABLE "UserModel".messages TO "restchat";
 
 CREATE TABLE "UserModel".users (
+	id integer NOT NULL,
 	username varchar NOT NULL,
-	passwordhash varchar NOT NULL,
-	id int4 NOT NULL
+	passwordhash varchar NOT NULL
 );
+
 CREATE INDEX users_id_idx ON "UserModel".users USING btree (id);
 
 ALTER TABLE "UserModel".users OWNER TO "restchat";
+
 GRANT ALL ON TABLE "UserModel".users TO "restchat";
 
 GRANT ALL ON SCHEMA "UserModel" TO "restchat";
 
 INSERT INTO "UserModel".users (username,passwordhash,id) VALUES
 	 ('Andrey','ertdfghjuehjuik345535',1);
+
 INSERT INTO "UserModel".messages (id,"text",user_id,"time") VALUES
 	 (1,'Привет всем',1,'2022-12-02 15:57:00+03');
