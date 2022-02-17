ae02bf71d3ada278f0eeba8d6f7388e207039221
min0ru <2010min0ru@gmail.com>
2022-02-17 21:55:07 +0300

    Исправлено ограничение на максимальную длину логина.

--- client/src/restrictions.js
+++ client/src/restrictions.js
@@ -1,6 +1,6 @@
 // TODO: Опционально импортировать часть настроек через параметры окружения.
 
-export const MAX_LOGIN_LENGTH = 15;
+export const MAX_LOGIN_LENGTH = 16;
 export const LOGIN_REGEX = /^([a-zA-Z0-9_-]){1,}$/;
 export const MAX_PASSWORD_LENGTH = 32;
 export const PASSWORD_REGEX = /^([a-zA-Z0-9_-]){1,}$/;
