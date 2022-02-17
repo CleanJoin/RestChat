11aa51d8b6d602b134cf449fa1f3edeba8b59b4b
min0ru <2010min0ru@gmail.com>
2022-02-17 21:54:42 +0300

    Добавлена новая настройка, максимальная длина сообщения.

---
+++ client/src/restrictions.js
@@ -0,0 +1,8 @@
+// TODO: Опционально импортировать часть настроек через параметры окружения.
+
+export const MAX_LOGIN_LENGTH = 15;
+export const LOGIN_REGEX = /^([a-zA-Z0-9_-]){1,}$/;
+export const MAX_PASSWORD_LENGTH = 32;
+export const PASSWORD_REGEX = /^([a-zA-Z0-9_-]){1,}$/;
+export const MAX_MESSAGE_LENGTH = 1024;
+export const MAX_MESSAGES_NUMBER = 100;
